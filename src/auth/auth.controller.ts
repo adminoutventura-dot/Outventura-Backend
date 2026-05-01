9f6a84f058d2ef48ef43362ec24684ba6f2fd8fd
Carolina <caroolina2.14@gmail.com>
2026-05-01 13:21:22 +0200

    Updated Swagger configuration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,9 @@
-// auth.controller.ts
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
-@ApiTags('auth')
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
--- src/main.ts
+++ src/main.ts
@@ -14,15 +14,30 @@ async function bootstrap() {
     transform: true,
   }));
 
+
   const config = new DocumentBuilder()
-    .setTitle('API Backend')
-    .setDescription('Documentació de la API REST')
+    .setTitle('Outventura API')
+    .setDescription('API per a la gestió d\'activitats i material de l\'app Outventura')
     .setVersion('1.0')
+    .addTag('Auth', 'Accés i seguretat')
+    .addTag('Roles (for User Management)', 'Rols i permisos')
+    .addTag('Users', 'Usuaris del sistema')
+    .addTag('Categories', 'Categories d\'activitats i material')
+    .addTag('Activities', 'Catàleg d\'activitats')
+    .addTag('Equipment', 'Inventari de material')
+    .addTag('Equipment Status', 'Estat del material')
+    // .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      defaultModelsExpandDepth: -1,
+    },
+  });
 
   await app.listen(3000);
+  console.log(`🚀 API funcionant en: http://localhost:3000/api`);
 }
 bootstrap();
