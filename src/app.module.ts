f3bf32f465c5e543523e46e4b1142d709d437b33
Carolina <caroolina2.14@gmail.com>
2026-05-16 13:00:03 +0200

    Create schema for Booking, BookingStatus, and Booking line.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,24 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ActivityModule } from './activity/activity.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { BookingLineModule } from './booking-line/booking-line.module';
+import { BookingStatusModule } from './booking-status/booking-status.module';
+import { BookingModule } from './booking/booking.module';
+import { CategoryModule } from './category/category.module';
+import { EquipmentStatusModule } from './equipment-status/equipment-status.module';
+import { EquipmentModule } from './equipment/equipment.module';
+import { GuideModule } from './guide/guide.module';
+import { ContentTypeMiddleware } from './middleware/content-type.middleware';
+import { LoggerMiddleware } from './middleware/logger.middleware';
+import { MaintenanceMiddleware } from './middleware/maintenance.middleware';
+import { SecurityHeadersMiddleware } from './middleware/security-headers.middleware';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 import { RoleModule } from './role/role.module';
 import { UserModule } from './user/user.module';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { EquipmentModule } from './equipment/equipment.module';
-import { EquipmentStatusModule } from './equipment-status/equipment-status.module';
-import { CategoryModule } from './category/category.module';
-import { ActivityModule } from './activity/activity.module';
-import { AuthModule } from './auth/auth.module';
-import { LoggerMiddleware } from './middleware/logger.middleware';
-import { SecurityHeadersMiddleware } from './middleware/security-headers.middleware';
-import { MaintenanceMiddleware } from './middleware/maintenance.middleware';
-import { ContentTypeMiddleware } from './middleware/content-type.middleware';
-import { GuideModule } from './guide/guide.module';
 
 @Module({
   imports: [
@@ -28,6 +31,9 @@ import { GuideModule } from './guide/guide.module';
     EquipmentStatusModule,
     CategoryModule,
     ActivityModule,
+    BookingModule,
+    BookingStatusModule,
+    BookingLineModule,
     AuthModule,
   ],
   controllers: [AppController],
