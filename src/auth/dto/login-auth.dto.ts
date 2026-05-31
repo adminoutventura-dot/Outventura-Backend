40e9215ff8c8e9dc779142da5b82b673e01cab22
Carolina <caroolina2.14@gmail.com>
2026-05-31 10:02:43 +0200

    Fix(validation): add custom error messages for email and password fields in LoginAuthDto

--- src/auth/dto/login-auth.dto.ts
+++ src/auth/dto/login-auth.dto.ts
@@ -3,12 +3,12 @@ import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class LoginAuthDto {
     @ApiProperty({ example: 'usuari@email.com' })
-    @IsEmail()
+    @IsEmail({}, { message: 'Introdueix un correu electrònic vàlid' })
     email!: string;
 
     @ApiProperty({ example: '12345678' })
     @IsString()
     @IsNotEmpty()
-    @MinLength(8)
+    @MinLength(8, { message: 'La contrasenya ha de tenir mínim 8 caràcters' })
     password!: string;
 }
